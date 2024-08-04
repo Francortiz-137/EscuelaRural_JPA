44db9d8f038735a8492980b3068927af4a149b50
Francortiz-137 <francortiz.c@gmail.com>
2024-08-04 05:09:14 -0400

    feat: students CRUD

--- src/main/java/cl/praxis/escuelarural_jpa/controller/StudentController.java
+++ src/main/java/cl/praxis/escuelarural_jpa/controller/StudentController.java
@@ -1,11 +1,11 @@
 package cl.praxis.escuelarural_jpa.controller;
 
+import cl.praxis.escuelarural_jpa.entity.Student;
 import cl.praxis.escuelarural_jpa.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/students")
@@ -22,6 +22,46 @@ public class StudentController {
     public String getStudents(Model model) {
         model.addAttribute("students", studentService.getAllStudents());
         return "students";
+    }
+
+    @GetMapping("/{id}")
+    public String getStudent(@PathVariable Long id, Model model) {
+        Student student = studentService.getStudent(id);
+        model.addAttribute("student", student);
+        return "student";
+    }
+
+    @GetMapping("/new")
+    public String showForm(Model model) {
+        model.addAttribute("student", new Student());
+        return "student_form";
+    }
+
+    @PostMapping("")
+    public String saveStudent(@ModelAttribute("student") Student student) {
+        studentService.addStudent(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        Student student = studentService.getStudent(id);
+        model.addAttribute("student", student);
+        return "student_edit_form";
+    }
+
+    @PutMapping("/edit/{id}")
+    public String updateStudent(@PathVariable Long id,
+                                @ModelAttribute("student") Student student) {
+        student.setId(id);
+        studentService.updateStudent(student);
+        return "redirect:/students";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable Long id, Model model) {
+        studentService.deleteStudent(id);
+        return "redirect:/students";
     }
 
 }
